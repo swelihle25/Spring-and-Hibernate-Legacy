ee3e09a9071d3de8b3509b32d864bda7efb0b798
swkhuzw022 <swkhuzw022@student.wethinkcode.co.za>
2026-01-12 11:32:36 +0200

    constructor injection

--- src/main/java/spring/annotations/TennisCoach.java
+++ src/main/java/spring/annotations/TennisCoach.java
@@ -8,11 +8,22 @@ public class TennisCoach implements Coach{
 
     private FortuneService fortuneService;
 
+    //define a default constructor
+    public TennisCoach(){
+        System.out.println(">> TennisCoach: inside default constructor");
+    }
+    @Autowired
+    public void setFortuneService(FortuneService theFortuneService){
+        System.out.println(">>TennisCoach: inside setFortuneService method");
+        fortuneService = theFortuneService;
+    }
+
+/*
     @Autowired
     public TennisCoach(FortuneService theFortuneService){
         fortuneService = theFortuneService;
     }
-
+*/
     @Override
     public String getDailyWorkOut() {
         return "Practice your backhand volley";
